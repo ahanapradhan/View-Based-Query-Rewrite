0d11dd6a44fcb617bb25e59034228e3525a0fdee
Ahana <sendatahana@gmail.com>
2024-09-04 15:53:32 +0530

    Update test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -35,11 +35,11 @@ select c_name, c_nationkey, n_name, n_nationkey from customer FULL OUTER JOIN na
 select c_name, n_name, s_name from customer, nation, supplier where c_nationkey = n_nationkey and n_nationkey = s_nationkey and c_acctbal < 5000 and s_acctbal > 9999;
 
 -- V1
-select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey and c_acctbal between 4001 and 4500 and s_acctbal > 10000;
+select c_name, c_nationkey, s_name, s_nationkey, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey and c_acctbal between 4001 and 4500 and s_acctbal > 10000;
 -- V2
-select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey and c_acctbal between 0 and 4000 and s_acctbal between 8000 and 100001;
+select c_name, c_nationkey, s_name, n_nationkey, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey and c_acctbal between 0 and 4000 and s_acctbal between 8000 and 100001;
 -- V3
-select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey and c_acctbal between 4501 and 7500 and s_acctbal between 5000 and 20000;
+select c_name, c_nationkey, s_name, n_name from customer, supplier, nation where c_nationkey = s_nationkey and c_nationkey = n_nationkey and c_acctbal between 4501 and 7500 and s_acctbal between 5000 and 20000;
 
 
 
