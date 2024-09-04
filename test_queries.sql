8aa51d37ab991bfdb58f7bf0c616d04e4cf241e5
Ahana <sendatahana@gmail.com>
2024-09-04 15:55:55 +0530

    Update test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -2,10 +2,13 @@
 Select c_name, s_name, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey;
 
 -- V1:
+Create view V1 as
 Select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey;
 -- V2:
+Create view V2 as
 Select c_name, c_nationkey, n_name from customer, nation where c_nationkey = n_nationkey;
 -- V3
+Create view V3 as
 Select c_name, c_nationkey from customer;
 
 
@@ -13,10 +16,13 @@ Select c_name, c_nationkey from customer;
 select c_name, n_name, s_name from customer, nation, supplier where c_nationkey = n_nationkey and n_nationkey = s_nationkey and c_acctbal < 1000;
 
 -- V1
+Create view V1 as
 Select c_name, c_nationkey from customer where c_acctbal < 1000;
 -- V2
+Create view V2 as
 select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey and c_acctbal < 2500;
 -- V3
+Create view V3 as
 select c_name, c_nationkey, n_name, n_nationkey from customer, nation where c_nationkey = n_nationkey and c_acctbal > 2500;
 
 
@@ -24,10 +30,13 @@ select c_name, c_nationkey, n_name, n_nationkey from customer, nation where c_na
 select c_name, n_name, s_name from customer, nation, supplier where c_nationkey = n_nationkey and n_nationkey = s_nationkey and c_acctbal < 1000;
 
 -- V1
+Create view V1 as
 Select c_name, c_nationkey from customer where c_acctbal < 1000;
 -- V2
+Create view V2 as
 select c_name, c_nationkey, s_name, s_nationkey from customer LEFT OUTER JOIN supplier ON c_nationkey = s_nationkey and c_acctbal < 2500;
 -- V3
+Create view V3 as
 select c_name, c_nationkey, n_name, n_nationkey from customer FULL OUTER JOIN nation ON c_nationkey = n_nationkey and c_acctbal > 2500;
 
 
@@ -35,10 +44,13 @@ select c_name, c_nationkey, n_name, n_nationkey from customer FULL OUTER JOIN na
 select c_name, n_name, s_name from customer, nation, supplier where c_nationkey = n_nationkey and n_nationkey = s_nationkey and c_acctbal < 5000 and s_acctbal > 9999;
 
 -- V1
+Create view V1 as
 select c_name, c_nationkey, s_name, s_nationkey, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey and c_acctbal between 4001 and 4500 and s_acctbal > 10000;
 -- V2
+Create view V2 as
 select c_name, c_nationkey, s_name, n_nationkey, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey and c_acctbal between 0 and 4000 and s_acctbal between 8000 and 100001;
 -- V3
+Create view V3 as
 select c_name, c_nationkey, s_name, n_name from customer, supplier, nation where c_nationkey = s_nationkey and c_nationkey = n_nationkey and c_acctbal between 4501 and 7500 and s_acctbal between 5000 and 20000;
 
 
