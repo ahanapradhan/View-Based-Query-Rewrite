6a1e10894e0c18d85c2664cf9e21a56c44021869
Ahana <sendatahana@gmail.com>
2024-09-04 14:42:04 +0530

    Q1 Update test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -1,1 +1,7 @@
+-- Q1:
+Select c_name, s_name, n_name from customer, supplier, nation where c_nationkey = s_nationkey and s_nationkey = n_nationkey;
 
+-- V1:
+Select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey;
+-- V2:
+Select c_name, c_nationkey, n_name, n_nationkey from customer, nation where c_nationkey = n_nationkey;
