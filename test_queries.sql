fceb06ce9e67545411921b284273e4c54629614a
Ahana <sendatahana@gmail.com>
2024-09-04 14:42:40 +0530

    Update test_queries.sql

--- test_queries.sql
+++ test_queries.sql
@@ -5,3 +5,6 @@ Select c_name, s_name, n_name from customer, supplier, nation where c_nationkey 
 Select c_name, c_nationkey, s_name, s_nationkey from customer, supplier where c_nationkey = s_nationkey;
 -- V2:
 Select c_name, c_nationkey, n_name, n_nationkey from customer, nation where c_nationkey = n_nationkey;
+-- V3
+Select c_name, c_nationkey from customer;
+
